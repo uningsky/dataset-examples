4391bb0d528e8639ef6ad421384a3e266b0e3f13
uningsky <uningsky@hotmail.com>
2019-01-03 01:09:25 +0900

    python3

--- json_to_csv_converter.py
+++ json_to_csv_converter.py
@@ -12,10 +12,10 @@ import simplejson as json
 
 def read_and_write_file(json_file_path, csv_file_path, column_names):
     """Read in the json dataset file and write it out to a csv file, given the column names."""
-    with open(csv_file_path, 'wb+') as fout:
+    with open(csv_file_path, 'w', encoding = 'utf-8') as fout:
         csv_file = csv.writer(fout)
         csv_file.writerow(list(column_names))
-        with open(json_file_path) as fin:
+        with open(json_file_path, encoding = "utf-8") as fin:
             for line in fin:
                 line_contents = json.loads(line)
                 csv_file.writerow(get_row(line_contents, column_names))
@@ -23,7 +23,7 @@ def read_and_write_file(json_file_path, csv_file_path, column_names):
 def get_superset_of_column_names_from_file(json_file_path):
     """Read in the json dataset file and return the superset of column names."""
     column_names = set()
-    with open(json_file_path) as fin:
+    with open(json_file_path, encoding = "utf-8") as fin:
         for line in fin:
             line_contents = json.loads(line)
             column_names.update(
@@ -49,7 +49,7 @@ def get_column_names(line_contents, parent_key=''):
 
     """
     column_names = []
-    for k, v in line_contents.iteritems():
+    for k, v in line_contents.items():
         column_name = "{0}.{1}".format(parent_key, k) if parent_key else k
         if isinstance(v, collections.MutableMapping):
             column_names.extend(
@@ -83,6 +83,8 @@ def get_nested_value(d, key):
     if base_key not in d:
         return None
     sub_dict = d[base_key]
+    if sub_dict is None:
+        return None
     return get_nested_value(sub_dict, sub_key)
 
 def get_row(line_contents, column_names):
@@ -93,8 +95,8 @@ def get_row(line_contents, column_names):
                         line_contents,
                         column_name,
                         )
-        if isinstance(line_value, unicode):
-            row.append('{0}'.format(line_value.encode('utf-8')))
+        if isinstance(line_value, str):
+            row.append('{0}'.format(line_value))
         elif line_value is not None:
             row.append('{0}'.format(line_value))
         else:
